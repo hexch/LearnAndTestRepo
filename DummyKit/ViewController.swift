9d4687a1d15dd6bdce122f1b289a5238fbc7f2de
何　暁川 <hexch.c@gmail.com>
2020-09-14 11:47:52 +0900

    Initial Commit

---
+++ DummyKit/ViewController.swift
@@ -0,0 +1,21 @@
+////  ViewController.swift
+//  DummyKit
+//
+//  Created by hexch on 2020/09/14.
+//  github:https://github.com/hexch
+//
+//  Copyright © 2020 hexch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
